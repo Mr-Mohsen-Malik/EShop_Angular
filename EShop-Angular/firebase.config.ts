0048bbd83b8bc7df85b33e3e14420c38323398ba
Mohsen Malik <48989392+Mr-Mohsen-Malik@users.noreply.github.com>
2021-02-21 18:11:07 +0530

    Delete firebase.config.ts

--- EShop-Angular/firebase.config.ts
+++
@@ -1,9 +0,0 @@
-export var firebaseConfig = {
-    apiKey: "AIzaSyBwHEyNPKf4G2QjsyNjrEW3Y9CQWeWqOec",
-    authDomain: "eshop-angular-8c3c7.firebaseapp.com",
-    projectId: "eshop-angular-8c3c7",
-    storageBucket: "eshop-angular-8c3c7.appspot.com",
-    messagingSenderId: "861905203897",
-    appId: "1:861905203897:web:4584d09b3e0a1c9427e168",
-    measurementId: "G-8XRDBPYX9T"
-  };
